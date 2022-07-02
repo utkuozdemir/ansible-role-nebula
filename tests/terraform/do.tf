41b66fee688f8272af4716d34588f2c053d280cc
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2022-07-02 15:10:58 +0200

    test: allow concurrent test runs by adding random id to droplets

--- tests/terraform/do.tf
+++ tests/terraform/do.tf
@@ -12,10 +12,15 @@ provider "digitalocean" {
   token = trimspace(file(".do-token.txt"))
 }
 
+resource "random_id" "id" {
+  # hex does bytes X 2, so the actual id will be 4 chars long
+  byte_length = 2
+}
+
 resource "digitalocean_droplet" "droplet" {
   count    = var.num_droplets
   image    = var.image
-  name     = "${var.name_prefix}${count.index + 1}"
+  name     = "${var.name_prefix}${random_id.id.hex}-${count.index + 1}"
   region   = var.region
   size     = var.size
   ssh_keys = var.ssh_keys
@@ -24,8 +29,9 @@ resource "digitalocean_droplet" "droplet" {
 
 resource "local_file" "ansible_inventory" {
   content = templatefile("ansible_inventory.tpl", {
-    droplets = digitalocean_droplet.droplet.*
-    tags     = sort(distinct(flatten(digitalocean_droplet.droplet.*.tags)))
+    droplets    = digitalocean_droplet.droplet.*
+    tags        = sort(distinct(flatten(digitalocean_droplet.droplet.*.tags)))
+    name_prefix = var.name_prefix
     }
   )
   filename = "../inventories/do/hosts"
